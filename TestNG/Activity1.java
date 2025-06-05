c78d3ebe65ed8703644aaf0b56d976b60764e5fe
Rachanab-64 <rrachana270@gmail.com>
2025-06-05 09:24:19 +0530

    Add files via upload

---
+++ TestNG/Activity1.java
@@ -0,0 +1,28 @@
+package suiteExample;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class Activity1 {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        
+
+        driver = new FirefoxDriver();
+        driver.get("https://training-support.net");
+    }
+
+    @Test
+    public void getTitleTest() {
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+}
